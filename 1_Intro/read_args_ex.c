025e654cfad999b4a57571443836e9572f6b7c4b
Christian DeLozier <crdelozier@gmail.com>
2014-09-19 12:55:59 -0400

    Switched sections 2 and 3

--- 1_Intro/read_args_ex.c
+++ 1_Intro/read_args_ex.c
@@ -3,13 +3,13 @@
 
 int main(int argc, char **argv) {
   if (argc != 2) {
-    printf("Usage: %s [true|false]", argv[0]);
+    printf("Usage: %s [number]\n", argv[0]);
   } else {
     int val = atoi(argv[1]);
     if (val == 100) {
       printf("That's a good number!\n");
     } else {
-      printf("%d is a bad number!\n", val);
+      printf("%d is a bad number! Do not enter %d!\n", val);
     }
   }
   return 0;
