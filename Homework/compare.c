507009a5ad16fd4f186ab5b04d0edace1385c835
Christian DeLozier <crdelozier@gmail.com>
2014-09-21 14:02:23 -0400

    Removed compare

--- Homework/compare.c
+++ Homework/compare.c
@@ -1,14 +1,4 @@
 #include "compare.h"
 
 int int_compare(void *p1, void *p2){
-  int i1 = (int)p1;
-  int i2 = (int)p2;
-
-  if(i1 > i2){
-    return 1;
-  }else if(i1 < i2){
-    return -1;
-  }else{
-    return 0;
-  }
 }
